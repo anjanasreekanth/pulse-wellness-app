403d84626fceffa13b05e75ffe63e6eae0a254ad
Anjana sreekanth <anjanavshenoy@gmail.com>
2026-09-13 18:11:07 -0500

    Added weekly goal repository and controller

--- pulse-api/src/main/java/com/example/pulse_api/goal/WeeklyGoal.java
+++ pulse-api/src/main/java/com/example/pulse_api/goal/WeeklyGoal.java
@@ -1,4 +1,67 @@
 package com.example.pulse_api.goal;
 
+import com.example.pulse_api.user.User;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(
+        name = "weekly_goal",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "week_start"})
+)
 public class WeeklyGoal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate weekStart;
+
+    @Column(nullable = false)
+    private Integer targetActivities;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public WeeklyGoal(){
+
+    }
+
+    public WeeklyGoal(LocalDate weekStart, Integer targetActivities, User user) {
+        this.weekStart = weekStart;
+        this.targetActivities = targetActivities;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+
+
+    public LocalDate getWeekStart() {
+        return weekStart;
+    }
+
+    public void setWeekStart(LocalDate weekStart) {
+        this.weekStart = weekStart;
+    }
+
+    public Integer getTargetActivities() {
+        return targetActivities;
+    }
+
+    public void setTargetActivities(Integer targetActivities) {
+        this.targetActivities = targetActivities;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
---
+++ pulse-api/src/main/java/com/example/pulse_api/goal/WeeklyGoalController.java
@@ -0,0 +1,71 @@
+package com.example.pulse_api.goal;
+
+import com.example.pulse_api.user.User;
+import com.example.pulse_api.user.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+
+@RestController
+@RequestMapping("/api/v1/users/{userId}/goals")
+public class WeeklyGoalController {
+    private final WeeklyGoalRepository weeklyGoalRepository;
+    private final UserRepository userRepository;
+
+    public WeeklyGoalController(WeeklyGoalRepository weeklyGoalRepository,
+                                UserRepository userRepository) {
+        this.weeklyGoalRepository = weeklyGoalRepository;
+        this.userRepository = userRepository;
+
+    }
+
+    @GetMapping("/current")
+    public WeeklyGoal getCurrentGoal(@PathVariable Long userId) {
+        LocalDate currentWeekStart = LocalDate.now()
+                .with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
+        return weeklyGoalRepository.findByUserIdAndWeekStart(userId, currentWeekStart)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "weekly goal not found"));
+    }
+
+    private User findUser(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "User not found"
+                ));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public WeeklyGoal createGoal(@PathVariable Long userId,
+                                 @RequestBody WeeklyGoal weeklyGoal) {
+        User user = findUser(userId);
+        weeklyGoal.setUser(user);
+        return weeklyGoalRepository.save(weeklyGoal);
+    }
+
+    private WeeklyGoal findGoal(Long userId, Long goalId) {
+        WeeklyGoal weeklyGoal = weeklyGoalRepository.findById(goalId)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "Weekly Goal not found"
+                ));
+        if (!weeklyGoal.getUser().getId().equals(userId)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Weekly goal not found");
+        }
+        return weeklyGoal;
+    }
+
+    @PutMapping("/{goalId}")
+    public WeeklyGoal updateGoal(@PathVariable Long userId,
+                                 @PathVariable Long goalId,
+                                 @RequestBody WeeklyGoal updatedGoal) {
+        WeeklyGoal existingGoal = findGoal(userId, goalId);
+        existingGoal.setTargetActivities(updatedGoal.getTargetActivities());
+        return weeklyGoalRepository.save(existingGoal);
+    }
+
+
+}
